878c789c5e8b9cf7c7239d02def6c45fe46e2c4d
eidjbehe <eidjbehe@gmail.com>
2026-03-29 03:54:24 +0300

    Update ESLint config and add unused function

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    ignores: ["dist/**", "coverage/**", "node_modules/**"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: {
+      globals: globals.node,
+    },
+    plugins: {
+      js,
+    },
+    extends: ["js/recommended"],
+  },
+  ...tseslint.configs.recommended,
+]);
